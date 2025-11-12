286c82bf7eabdb74591cf37c7fc0a5257fedcb76
TitouanPerros <titouan.perros02@edu.univ-fcomte.fr>
2025-11-12 17:11:50 +0100

    ouverture/fermeture menu

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+const toggle = document.querySelector(".menu-btn");
+const nav = document.querySelector("nav.menu");
+
+toggle.addEventListener("click", () => {
+  const isOpen = toggle.ariaExpanded === "true";
+  const isClosed = !isOpen;
+  console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+
+  if (isOpen) {
+    // Fermer
+    nav.ariaHidden = "true";
+    toggle.ariaExpanded = "false";
+    document.body.classList.remove("noscroll");
+  } else {
+    // Ouvrir
+    nav.ariaHidden = "false";
+    toggle.ariaExpanded = "true";
+    document.body.classList.add("noscroll");
+  }
+});
